2301e84a8c93a838d31d4f68150b4480bc4308a5
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-01-19 15:15:18 +0530

    fixed error

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -8,7 +8,7 @@ import WorkProcess from "./components/WorkProcessPage";
 import ESolutionsSection from "./components/ESolutionsSection";
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gradient-to-b from-[#FFF5EB] to-white'>
+    <div className='min-h-screen bg-[#FAF4E7]'>
       <Navbar />
       <OnboardingPage />
       <ComprehensiveServices />
