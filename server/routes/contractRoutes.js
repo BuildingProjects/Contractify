67385ad1f1498a74e8c3c31d987ab10a25ab9ff9
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-02-08 00:24:28 +0530

    created route for sending mail upon contract generation and route for pdf download

--- server/routes/contractRoutes.js
+++ server/routes/contractRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createContract } = require("../controllers/contractController");
-const {verifyToken} = require("../utils/jwtHelper");
+const {
+  createContract,
+  getContractsByEmail,
+  acceptContract,
+  rejectContract,
+  generateContractPDF,
+} = require("../controllers/contractController");
+const { verifyToken } = require("../utils/jwtHelper");
 const router = express.Router();
 
 router.post("/createContract", verifyToken, createContract);
+router.get("/getContracts/:email", verifyToken, getContractsByEmail);
+router.get("/acceptContract/:id", acceptContract);
+router.get("/rejectContract/:id", rejectContract);
+router.get("/generatePDF/:id", generateContractPDF);
 
 module.exports = router;
