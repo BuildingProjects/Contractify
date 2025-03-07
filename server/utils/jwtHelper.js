36e6f2d7c80fcbe3b1bb51417f1b0790ca7072dd
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2025-03-07 19:40:56 +0530

    increased cookie and jwt time

--- server/utils/jwtHelper.js
+++ server/utils/jwtHelper.js
@@ -5,7 +5,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 
 module.exports = {
   generateToken: (payload) =>
-    jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" }),
+    jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" }), 
   verifyToken: (token) => jwt.verify(token, JWT_SECRET),
 };
 
