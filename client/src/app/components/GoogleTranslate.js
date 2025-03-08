27eba1445f5164fe805aa7976ccb3d2ca2a03798
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-03-08 12:47:02 +0530

    added google translate on website

---
+++ client/src/app/components/GoogleTranslate.js
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from "react";
+
+export default function GoogleTranslate() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement(
+        { pageLanguage: "en" },
+        "google_translate_element"
+      );
+    };
+  }, []);
+
+  return <div id="google_translate_element"></div>;
+}
