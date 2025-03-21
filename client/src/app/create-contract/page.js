b0d6c4b7eb3bfcda0c7e3d2491b6aa29fe431c3c
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-03-21 13:15:00 +0530

    added the code

--- client/src/app/create-contract/page.js
+++ client/src/app/create-contract/page.js
@@ -478,7 +478,7 @@ export default function CreateContractPage() {
                   className={`w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
                     errors.endDate ? "border-red-500" : "border-gray-300"
                   }`}
-                  ref={endDateInputRef} // Add ref to access the input element
+                  ref={endDateInputRef}
                 />
                 <CalendarIcon
                   className='absolute right-3 top-2.5 h-5 w-5 text-gray-400 cursor-pointer'
